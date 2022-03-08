4c922fd022f24e8ddb8b1a131e6656ea8d90acb2
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-03-09 01:52:17 +0530

    project-init

---
+++ src/main/java/com/ax/SpringBootJWT/SpringBootJwtApplication.java
@@ -0,0 +1,13 @@
+package com.ax.SpringBootJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ax/SpringBootJWT/SpringBootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ax.SpringBootJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
